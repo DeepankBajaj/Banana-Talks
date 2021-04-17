9701ec35595849032fa1f6909ec116b857853aaa
Deepank <deepankbajaj1999@gmail.com>
2021-04-17 15:40:40 +0530

    Added css

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-//input
-var username = prompt("Give me your name")
+var inputBox = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputBox = document.querySelector("#txt-output");
 
-//processing
-var welcomeMessage = "this sccript works" + username;
+var url = "https://api.funtranslations.com/translate/minion.json";
 
-//output
-alert(welcomeMessage);
-    
+function addText(text) {
+
+    return url + '?' + 'text=' + text;
+}
+
+function clickHandler() {
+
+    var inputText = inputBox.value;
+    var urlTxt = addText(inputText);
+  
+    fetch(urlTxt)
+        .then(response => response.json())
+        .then(json => outputBox.innerHTML = json.contents.translated);
+}
+
+btnTranslate.addEventListener('click', clickHandler);
